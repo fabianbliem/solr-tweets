18a56272bd1444b3b21cc473fd23ed3d86e45988
Maria Edlinger <medlinger.mmt-b2018@fh-salzburg.ac.at>
2020-06-19 01:14:12 +0200

    Lint Code

--- src/index.js
+++ src/index.js
@@ -6,20 +6,20 @@ import './style/main.scss';
 const solr = new Solr();
 
 const suchen = async (line, filter, checkbox) => {
-  const results = await solr.search(line.trim(),0,100,filter, checkbox);
+  const results = await solr.search(line.trim(), 0, 100, filter, checkbox);
   return results;
-}
+};
 
 const vorschlagen = async (line) => {
   const results = await solr.recom(line.trim());
   return results.suggestions[1].suggestion;
-}
+};
 
-const getFilter = (from,to) =>{
-  const fromFilter= 1478649600000 + from[0]*3600000 + from[1]*60000 + from[2]*1000;
-  const toFilter= 1478649600000 + to[0]*3600000 + to[1]*60000 + to[2]*1000;
-  return "timestamp_ms:["+fromFilter+" TO "+toFilter+"]";
-}
+const getFilter = (from, to) => {
+  const fromFilter = 1478649600000 + from[0] * 3600000 + from[1] * 60000 + from[2] * 1000;
+  const toFilter = 1478649600000 + to[0] * 3600000 + to[1] * 60000 + to[2] * 1000;
+  return `timestamp_ms:[${fromFilter} TO ${toFilter}]`;
+};
 
 function boldString(str, find) {
   const reg = new RegExp(`(${find})`, 'gi');
@@ -37,12 +37,10 @@ const highlight = (text, search) => {
 };
 
 function initSearch() {
-
-  const dayButton= document.querySelector('.btn-day');
+  const dayButton = document.querySelector('.btn-day');
 
 
   dayButton.addEventListener('click', async () => {
-
     const input = document.getElementById('text');
     let filterInputFrom = document.getElementById('from_date').value;
     let filterInputTo = document.getElementById('to_date').value;
@@ -54,20 +52,20 @@ function initSearch() {
     const checkbox = document.getElementById('sortby');
     let results;
     let suchwort = input.value;
-    results = await suchen(suchwort,filter,checkbox.checked);
+    results = await suchen(suchwort, filter, checkbox.checked);
 
-    document.getElementById("resultsheading").innerHTML = "Results:\t"+results.numFound;
+    document.getElementById('resultsheading').innerHTML = `Results:\t${results.numFound}`;
 
-    let outDiv = document.getElementById('output');
+    const outDiv = document.getElementById('output');
 
-    outDiv.innerHTML = ""
+    outDiv.innerHTML = '';
 
     output(outDiv, suchwort, results);
 
-    if(results.numFound <= 3){
+    if (results.numFound <= 3) {
       const recomResults = await vorschlagen(suchwort);
       outDiv.innerHTML += `<h3>Meintest du: ${recomResults[0].word} </h3>`;
-      results = await suchen(recomResults[0].word,filter,checkbox.checked);
+      results = await suchen(recomResults[0].word, filter, checkbox.checked);
       suchwort = recomResults[0].word;
     }
 
@@ -81,17 +79,16 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-function output(outDiv, suchwort, results){
+function output(outDiv, suchwort, results) {
   for (const result of results.docs.slice(0, 10)) {
-     
-    outDiv.innerHTML += 
-    `<div class="card tweet">
+    outDiv.innerHTML
+    += `<div class="card tweet">
     <p class="upp">&#128579;</p>
     <span class="cardhead">
-      <p class="uname marked">${result[USERNAME_FIELD]}</p> <p class="tweetdate light">${result[TIME_FIELD][0].substring(0,19)}</p>
+      <p class="uname marked">${result[USERNAME_FIELD]}</p> <p class="tweetdate light">${result[TIME_FIELD][0].substring(0, 19)}</p>
     </span>
     <p class="tweet">${result[TEXT_FIELD]}</p>
     </div>`;
   }
-  outDiv.innerHTML = highlight(outDiv.innerHTML,suchwort);
+  outDiv.innerHTML = highlight(outDiv.innerHTML, suchwort);
 }
